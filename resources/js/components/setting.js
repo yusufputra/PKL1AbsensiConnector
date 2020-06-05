e43f4d23acd20bdef487170b863b8527aa02683f
yusufputra <yusufputra99@gmail.com>
2020-06-05 10:58:59 +0700

    deleting comments

--- resources/js/components/setting.js
+++ resources/js/components/setting.js
@@ -65,29 +65,6 @@ const setting = () => {
         }
     ];
 
-    // const data = [
-    //     {
-    //         key: "1",
-    //         name: "John Brown",
-    //         age: 32,
-    //         address: "New York No. 1 Lake Park",
-    //         tags: ["nice", "developer"]
-    //     },
-    //     {
-    //         key: "2",
-    //         name: "Jim Green",
-    //         age: 42,
-    //         address: "London No. 1 Lake Park",
-    //         tags: ["loser"]
-    //     },
-    //     {
-    //         key: "3",
-    //         name: "Joe Black",
-    //         age: 32,
-    //         address: "Sidney No. 1 Lake Park",
-    //         tags: ["cool", "teacher"]
-    //     }
-    // ];
     return (
         <div>
             <Breadcrumb style={{ margin: "16px 0" }}>
